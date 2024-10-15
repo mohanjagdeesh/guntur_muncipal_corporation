634a55477566f0177509a6bb4b127614d4a6c8ef
mohanjagdeesh <mohanjagdeesh5243.com>
2024-10-16 00:22:47 +0530

    Scroll to top functionality has implemented.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import Footer from './components/footer/Footer.tsx';
 import Header from './components/header/Header.tsx';
-import HomePage from './pages/home/HomePage.tsx'
-import './scss/main.scss'
+import HomePage from './pages/home/HomePage.tsx';
+import './scss/main.scss';
+import ScrollToTop from './global-components/scroll/scroll-to-top.tsx';
 
 function App() {
   return (
-    <div className=" bg-white h-screen">
-      <Header/>
-        <HomePage/>
-      <Footer/>
+    <div className="bg-white h-screen relative">
+      <Header />
+      <HomePage />
+      <Footer />
+      <ScrollToTop/>
     </div>
   );
 }
