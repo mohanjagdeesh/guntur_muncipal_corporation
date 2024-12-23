a6ad6be75de59d24b76b0d79f75c814dfe60a903
mohanjagdeesh <mohanjagdeesh5243.com>
2024-12-23 23:36:34 +0530

    Added styles for departments.

--- src/global-components/departments-data-renderer/departments-data-renderer.tsx
+++ src/global-components/departments-data-renderer/departments-data-renderer.tsx
@@ -5,9 +5,9 @@ const DepartmentsDataRenderer = ({description,liRenderer,dataGrid,gridDescriptio
   return (
     <div>
         <div>
-          <div className='grid grid-cols-1 md:grid-cols-2 bg-blue-200 p-4 mt-2'>
+          <div className='grid grid-cols-1 md:grid-cols-2 bg-blue-200 p-4 mt-2 gap-4'>
             {departmentImage && <img className='h-[200px] md:h-[350px] my-4' src={departmentImage} alt='Department'  />}
-            <p className=' text-[0.8rem] md:text-[1.2rem] leading-tight mb-3 text-justify h-full flex items-center justify-center'>{description}</p>
+            <p className=' text-[0.8rem] md:text-[1.2rem] font-light leading-tight mb-3 text-justify h-full flex items-center justify-center'>{description}</p>
           </div>
           {liRenderer}
         </div>
